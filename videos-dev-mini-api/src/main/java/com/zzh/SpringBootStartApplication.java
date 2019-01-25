a488ac04e240de779883822ee2638ad96e577152
zzh <824497374@qq.com>
2019-01-25 15:52:10 +0800

    add prod

---
+++ videos-dev-mini-api/src/main/java/com/zzh/SpringBootStartApplication.java
@@ -0,0 +1,17 @@
+package com.zzh;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * @author ZZH
+ * @date 2019/1/25 15:02
+ **/
+public class SpringBootStartApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(Application.class);
+    }
+}
