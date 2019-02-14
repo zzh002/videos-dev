b46b414c8cc7c28e0e0295420d2996c11b72b75b
zzh <824497374@qq.com>
2019-02-14 15:53:42 +0800

    service

--- videos-dev-mini-api/src/main/java/com/zzh/WebMvcConfig.java
+++ videos-dev-mini-api/src/main/java/com/zzh/WebMvcConfig.java
@@ -16,8 +16,8 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**")
 		.addResourceLocations("classpath:/META-INF/resources/")
-//				.addResourceLocations("file:/developer/videos_dev/");
-				.addResourceLocations("file:D:/videos_dev/");
+				.addResourceLocations("file:/developer/videos_dev/");
+//				.addResourceLocations("file:D:/videos_dev/");
 	}
 
 	@Bean
