3c8af8877a3b1b130b1cd24aef708176d4c545f9
zzh <824497374@qq.com>
2019-03-28 17:16:51 +0800

    modify WXLogin

--- videos-dev-mini-api/src/main/java/com/zzh/controller/WXLoginController.java
+++ videos-dev-mini-api/src/main/java/com/zzh/controller/WXLoginController.java
@@ -3,37 +3,31 @@ package com.zzh.controller;
 import com.zzh.config.ProjectConfig;
 import com.zzh.utils.AesCbcUtil;
 import com.zzh.utils.HttpRequest;
+import com.zzh.utils.JSONResult;
 import org.activiti.engine.impl.util.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author ZZH
  * @date 2019/2/22 15:45
  **/
-@Controller
+@RestController
 @RequestMapping("/wXLogin")
 public class WXLoginController {
 
     @Autowired
     private ProjectConfig projectConfig;
 
-    @RequestMapping(params = "decodeUserInfo")
-    @ResponseBody
-    public Map decodeUserInfo(String encryptedData, String iv, String code) {
+    @RequestMapping("/decodeUserInfo")
+    public JSONResult decodeUserInfo(String encryptedData, String iv, String code) {
 
-        Map map = new HashMap();
+//        Map map = new HashMap();
 
         // 登录凭证不能为空
         if (code == null || code.length() == 0) {
-            map.put("status", 0);
-            map.put("msg", "code 不能为空");
-            return map;
+            return JSONResult.errorException("code 不能为空");
         }
 
         // 小程序唯一标识 (在微信小程序管理后台获取)
@@ -61,30 +55,36 @@ public class WXLoginController {
         try {
             String result = AesCbcUtil.decrypt(encryptedData, session_key, iv, "UTF-8");
             if (null != result && result.length() > 0) {
-                map.put("status", 1);
-                map.put("msg", "解密成功");
 
                 JSONObject userInfoJSON = new JSONObject(result);
-                Map userInfo = new HashMap();
-                userInfo.put("openId", userInfoJSON.get("openId"));
-                userInfo.put("nickName", userInfoJSON.get("nickName"));
-                userInfo.put("gender", userInfoJSON.get("gender"));
-                userInfo.put("city", userInfoJSON.get("city"));
-                userInfo.put("province", userInfoJSON.get("province"));
-                userInfo.put("country", userInfoJSON.get("country"));
-                userInfo.put("avatarUrl", userInfoJSON.get("avatarUrl"));
-                // 解密unionId & openId;
-                if (!userInfoJSON.isNull("unionId")) {
-                    userInfo.put("unionId", userInfoJSON.get("unionId"));
-                }
-                map.put("userInfo", userInfo);
+//                Map userInfo = new HashMap();
+                String nickname = (String) userInfoJSON.get("nickName");
+                Integer gender = (Integer) userInfoJSON.get("gender");
+                String city = (String) userInfoJSON.get("city");
+                String province = (String) userInfoJSON.get("province");
+                String country = (String) userInfoJSON.get("country");
+                String faceImage = (String) userInfoJSON.get("avatarUrl");
+
+//                userInfo.put("openId", userInfoJSON.get("openId"));
+//                userInfo.put("nickName", userInfoJSON.get("nickName"));
+//                userInfo.put("gender", userInfoJSON.get("gender"));
+//                userInfo.put("city", userInfoJSON.get("city"));
+//                userInfo.put("province", userInfoJSON.get("province"));
+//                userInfo.put("country", userInfoJSON.get("country"));
+//                userInfo.put("avatarUrl", userInfoJSON.get("avatarUrl"));
+//                // 解密unionId & openId;
+//                if (!userInfoJSON.isNull("unionId")) {
+//                    userInfo.put("unionId", userInfoJSON.get("unionId"));
+//                }
+//                map.put("userInfo", userInfo);
             } else {
-                map.put("status", 0);
-                map.put("msg", "解密失败");
+                return JSONResult.errorMsg("解密失败");
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }       	return map;
+            return JSONResult.errorException(e.getMessage());
+        }
+        return JSONResult.ok("解密成功");
     }
 
 }
