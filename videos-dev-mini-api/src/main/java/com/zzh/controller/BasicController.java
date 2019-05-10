1b5f874d84d0ea60eb381ab13c7c9cabcc0bcd1a
zzh <824497374@qq.com>
2019-05-10 10:21:03 +0800

    add aliyun

--- videos-dev-mini-api/src/main/java/com/zzh/controller/BasicController.java
+++ videos-dev-mini-api/src/main/java/com/zzh/controller/BasicController.java
@@ -15,12 +15,12 @@ public class BasicController {
 	public static final String ADMIN_SESSION = "Admin-session";
 	
 	// 文件保存的命名空间
-	public static final String FILE_SPACE = "D:/videos_dev";
-//	public static final String FILE_SPACE = "/developer/videos_dev";
+//	public static final String FILE_SPACE = "D:/videos_dev";
+	public static final String FILE_SPACE = "/developer/videos_dev";
 
 	// ffmpeg所在目录
-	public static final String FFMPEG_EXE = "D:/ffmpeg/bin/ffmpeg.exe";
-//	public static final String FFMPEG_EXE = "/developer/ffmpeg/bin/./ffmpeg";
+//	public static final String FFMPEG_EXE = "D:/ffmpeg/bin/ffmpeg.exe";
+	public static final String FFMPEG_EXE = "/developer/ffmpeg/bin/./ffmpeg";
 
 //	// 每页分页的记录数
 	public static final Integer PAGE_SIZE = 5;
