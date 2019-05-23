2b056de398a0f217e007cb88d57d2833bbd5ab4e
zzh <824497374@qq.com>
2019-05-23 11:45:05 +0800

    ffmpeg

--- videos-dev-mini-api/src/main/java/com/zzh/controller/BasicController.java
+++ videos-dev-mini-api/src/main/java/com/zzh/controller/BasicController.java
@@ -20,7 +20,7 @@ public class BasicController {
 
 	// ffmpeg所在目录
 //	public static final String FFMPEG_EXE = "D:/ffmpeg/bin/ffmpeg.exe";
-	public static final String FFMPEG_EXE = "/developer/ffmpeg/bin/./ffmpeg";
+	public static final String FFMPEG_EXE = "/developer/ffmpeg-4.1/./ffmpeg";
 
 //	// 每页分页的记录数
 	public static final Integer PAGE_SIZE = 5;
